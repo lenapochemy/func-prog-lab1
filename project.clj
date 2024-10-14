7877e7f2a648dc957235c108e881e44b1aabc82c
pochemylena <redmirise@gmail.com>
2024-10-14 21:52:41 +0300

    again try ci

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject lab-1 "0.1"
-  :dependencies [[org.clojure/clojure "1.10.3"]]
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [lambdaisland/kaocha "1.0.887"]]
   :main lab1
   :aliases 
   {:test {:extra-paths ["test"]
