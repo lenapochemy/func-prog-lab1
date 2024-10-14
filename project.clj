da3ffa4b3da3a0e5abf0a1d73968748fd082f336
pochemylena <redmirise@gmail.com>
2024-10-14 21:21:54 +0300

    ci try again

--- project.clj
+++ project.clj
@@ -1,7 +1,3 @@
 (defproject lab-1 "0.1"
   :dependencies [[org.clojure/clojure "1.10.3"]]
-  :main lab1
-  :aliases {:test {:exec ["clojure" "-A:test"]}
-            :lint {:exec ["clj-kondo" "--lint" "src" "--config" ".clj-kondo/config.edn"]}
-            :format {:exec ["clj-kondo" "--lint" "src" "--config" ".clj-kondo/config.edn" "--fix"]}}
- )
+  :main lab1)
